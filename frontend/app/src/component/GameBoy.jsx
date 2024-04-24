311c50186726fc3cf8cb37066d083225dd2ea2f6
ma21005 <hockey0513hockey@gmail.com>
2024-04-24 23:50:13 +0900

    front

--- frontend/app/src/component/GameBoy.jsx
+++ frontend/app/src/component/GameBoy.jsx
@@ -1,40 +1,78 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import WorkHistory from './WorkHistory';
 import cursorAudio from '../audio/cursor.mp3'
+import powerOnAudio from '../audio/screenPowerOn.mp3'
+import powerOffAudio from '../audio/screenPowerOff.mp3'
 import "../style/GameBoy.css";
 
 const GameBoy = () => {
   const [hoveredNum, setHoveredNum] = useState(1);
+  const [screenPower, setScreenPower] = useState(false);
+  const [screenStyle, setScreenStyle] = useState({});
 
+  const powerOn = new Audio(powerOnAudio);
+  const powerOff = new Audio(powerOffAudio);
   const cursor = new Audio(cursorAudio);
+
+  const settingButtonClick = () => {
+    if (screenPower) {
+      powerOff.play();
+    } else {
+      powerOn.play();
+    }
+    setScreenPower(prevScreenPower => !prevScreenPower);
+  };
 
   const topBottomClick = () => {
     setHoveredNum(prevNum => (prevNum > 1 ? prevNum - 1 : 5));
-    cursor.play();
+    if (screenPower) {
+      cursor.play();
+    }
   };
 
   const bottomBottomClick = () => {
     setHoveredNum(prevNum => (prevNum < 5 ? prevNum + 1 : 1));
-    cursor.play();
+    if (screenPower) {
+      cursor.play();
+    }
   };
+
+  useEffect(() => {
+    if (screenPower) {
+      setScreenStyle({
+        // 1秒かけて背景色をwhiteに変化させる
+        backgroundColor: 'white',
+        transition: 'background-color 1s ease'
+      });
+    }
+  }, [screenPower]); // screenPowerの変化時に再計算
 
   return (
     <div className="gba">
       <div className="gba-upper">
-        <div className="gba-screen">
-          {[1, 2, 3, 4, 5].map(num => (
-            <WorkHistory key={num} num={String(num)} isHovered={hoveredNum === num} />
-          ))}
-        </div>
+        { screenPower ? (
+          <div className="gba-screen-on" style={screenStyle}>
+            {/* {[1, 2, 3, 4, 5].map(num => (
+              <WorkHistory key={num} num={String(num)} isHovered={hoveredNum === num} />
+            ))} */}
+          </div>
+        ) : (
+          <div className="gba-screen-off">
+          </div>
+        ) }
       </div>
       <div className="gba-joint">
         <div className="gba-joint-line-1"></div>
         <div className="gba-joint-line-2"></div>
       </div>
       <div className="gba-lower">
-        <button className="setting-button"></button>
+        <button className="setting-button" onClick={settingButtonClick}></button>
         <div className="setting-circle"></div>
-        <div className="power-lamp-1"></div>
+        { screenPower ? (
+          <div className="power-lamp-1-on"></div>
+        ) : (
+          <div className="power-lamp-1-off"></div>
+        ) }
         <div className="power-lamp-2"></div>
         <div className="gba-dpad">
           <div className="cross-layout">
