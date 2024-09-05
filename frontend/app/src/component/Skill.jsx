5d2a9cf55538adbd130cee657fb0e766bd2cfcfd
ma21005 <hockey0513hockey@gmail.com>
2024-09-05 22:53:31 +0900

    熟練度に応じて星の画像を表示させる

--- frontend/app/src/component/Skill.jsx
+++ frontend/app/src/component/Skill.jsx
@@ -2,6 +2,8 @@ import { useState, useEffect } from 'react';
 import React from 'react';
 import axios from 'axios';
 
+import "../style/Skill.css";
+
 const Skill = (props) => {
     // バックエンドのAPIからjsonデータを取得
     const [skills, setSkills] = useState([]);
@@ -20,16 +22,31 @@ const Skill = (props) => {
     return (
       <div>
         <ul>
-        {skills.map(skill => (
-          <React.Fragment key={skill.id}>
-            {/* <li>{skill.skill_type}</li>
-            <li>{skill.experience}</li> */}
-            <div>
-              <p>{skill.skill_type}</p>
-              <p>{skill.experience}</p>
-            </div>
-          </React.Fragment>
-        ))}
+        {skills.map(skill => {
+          // 熟練度(skill.experience)の整数部分の数だけ全ての星を表示し
+          // 小数部分に応じて星を部分的に表示させるためにそれぞれ分ける
+          const fullStars = Math.floor(skill.experience);  // 整数部分
+          const partialStar = skill.experience - fullStars;  // 小数部分
+
+          return (
+            <React.Fragment key={skill.id}>
+              <div>
+                <h2>{skill.skill_type}</h2>
+                <p>{skill.experience}</p>
+                <div>
+                  {/* 整数部分に応じて星を表示 */}
+                  {Array.from({ length: fullStars }).map((_, index) => (
+                    <img className="skill_star" src="/img/skill_star.jpg" alt="Skill Star" />
+                  ))}
+                  {/* 小数部分に応じて星を部分的に表示 */}
+                  <img className="skill_star" src="/img/skill_star.jpg" alt="Skill Star" style={{  
+                        clipPath: `inset(0 ${100 - partialStar * 100}% 0 0)`
+                      }} />
+                </div>
+              </div>
+            </React.Fragment>
+          );
+        })}
         </ul>
       </div>
     );
