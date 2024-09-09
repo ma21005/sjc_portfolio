6f97de222ba3beabf19803cd358b098b3cf0ba3c
ma21005 <hockey0513hockey@gmail.com>
2024-09-10 00:20:39 +0900

    skillページのデザイン更新

--- frontend/app/src/component/Skill.jsx
+++ frontend/app/src/component/Skill.jsx
@@ -24,27 +24,38 @@ const Skill = (props) => {
         {skills.map(skill => {
           // 熟練度(skill.experience)の整数部分の数だけ全ての星を表示し
           // 小数部分に応じて星を部分的に表示させるためにそれぞれ分ける
-          const fullStars = Math.floor(skill.experience);  // 整数部分
-          const partialStar = skill.experience - fullStars;  // 小数部分
+          const skillExperience = Math.floor(skill.experience * 10) / 10;  // 小数点第二位以降を切り捨て
+          const fullStars = Math.floor(skillExperience);  // 整数部分
+          const partialStar = skillExperience - fullStars;  // 小数部分
 
           // スキル名(skill.skill_type)に基づく画像パス
           const imagePath = `/img/icons/${skill.skill_type}.png`;
 
           return (
             <React.Fragment key={skill.id}>
-              <div className="skill_frame nes-container is-rounded is-dark">
-                <img className="skill_icon" src={imagePath} alt={skill.skill_type} />
-                <p className="skill_name">{skill.skill_type}</p>
-                <p>{skill.experience}</p>
-                <div className="skill_level_frame">
-                  {/* 整数部分に応じて星を表示 */}
-                  {Array.from({ length: fullStars }).map((_, index) => (
-                    <img key={index} className="skill_star" src="/img/skill_star.jpg" alt="Skill Star" />
-                  ))}
-                  {/* 小数部分に応じて星を部分的に表示 */}
-                  <img className="skill_star" src="/img/skill_star.jpg" alt="Skill Star" style={{  
-                        clipPath: `inset(0 ${100 - partialStar * 100}% 0 0)`
-                      }} />
+              <div className="skill_container nes-container is-rounded is-dark">
+                <div className="skill_icon_and_name_container">
+                  <div className="skill_icon_container nes-container is-rounded is-dark">
+                    <img className="skill_icon" src={imagePath} alt={skill.skill_type} />
+                  </div>
+                  <div className="skill_name_container">
+                    <p className="skill_name">{skill.skill_type}</p>
+                  </div>
+                </div>
+                <div className="skill_star_and_level_container">
+                  <div className="skill_star_container">
+                    {/* 整数部分に応じて星を表示 */}
+                    {Array.from({ length: fullStars }).map((_, index) => (
+                      <img key={index} className="skill_star" src="/img/skill_star.jpg" alt="Skill Star" />
+                    ))}
+                    {/* 小数部分に応じて星を部分的に表示 */}
+                    <img className="skill_star" src="/img/skill_star.jpg" alt="Skill Star" style={{
+                      clipPath: `inset(0 ${100 - partialStar * 100}% 0 0)`
+                    }} />
+                  </div>
+                  <div className="skill_level_container">
+                    <span>{skillExperience}</span>
+                  </div>
                 </div>
               </div>
             </React.Fragment>
