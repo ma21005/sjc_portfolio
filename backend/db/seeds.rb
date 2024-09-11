5f1d5ab02ce4c4357349528f8ac09fd36f37b05d
ma21005 <hockey0513hockey@gmail.com>
2024-09-11 22:39:15 +0900

    seedデータ変更

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -12,9 +12,10 @@ Profile.create(
 # Sample skills
 Skill.create([
   { skill_type: 'Ruby', experience: 3.0 },
-  { skill_type: 'Python', experience: 2 },
+  { skill_type: 'Python', experience: 2.0 },
   { skill_type: 'JavaScript', experience: 1.5 },
-  { skill_type: 'HTML', experience: 1.0 },
+  { skill_type: 'HTML', experience: 2.7 },
   { skill_type: 'CSS', experience: 5.0 },
+  { skill_type: 'Rails', experience: 3.0 },
   { skill_type: 'React', experience: 4.5 }
 ])
