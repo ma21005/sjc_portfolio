1b981579f96a6db2c004c92815f74e7c8871a0ee
ma21005 <hockey0513hockey@gmail.com>
2024-09-15 18:28:27 +0900

    リファクタ

--- frontend/app/src/component/GameBoy.jsx
+++ frontend/app/src/component/GameBoy.jsx
@@ -1,14 +1,11 @@
 import { useState, useEffect, useRef} from 'react';
-
 import WorkHistory from './WorkHistory';
-
 import Title from './Title'
 import Profile from './Profile';
 import Career from './Career';
 import Skill from './Skill';
 import Deliverable from './Deliverable';
 import ReturnTitle from './ReturnTitle';
-
 import backgroundAudio from '../audio/backgrond.mp3';
 import titleScreenButtonAudio from '../audio/titleScreenButton.mp3';
 import menuScreenButtonAudio from '../audio/menuScreenButton.mp3';
@@ -21,31 +18,33 @@ import "nes.css/css/nes.min.css";
 import "../style/GameBoy.css";
 
 const GameBoy = () => {
-  const [hoveredNum, setHoveredNum] = useState(1);
-  const [screenPower, setScreenPower] = useState(false);
-  const [screenStyle, setScreenStyle] = useState({});
 
-  // 画面を判定するためのState
+  // ========== 表示されている画面の判定、状態の保存を行うState ========== //
+
+  const [hoge, setHoge] = useState(true);
+  // タイトル画面
   const [titleScreen, setTitleScreen] = useState(true);
+  // メニュー画面
   const [menuScreen, setMenuScreen] = useState(false);
+  // メニュー画面に表示されている各項目の詳細画面
   const [detailScreen, setDetailScreen] = useState(false);
+  // タイトル画面に戻るかどうかを選べる画面（以降：リターン画面）
   const [showReturnTitle, setShowReturnTitle] = useState(false);
+
+  // 電源がオンかオフかを保存する
+  const [screenPower, setScreenPower] = useState(false);
+  // 電源オン時にタイトル画面をフェードインさせるため
+  const [screenStyle, setScreenStyle] = useState({});
+  // メニュー画面で現在どの項目を選択しているかを保存する
+  const [hoveredNum, setHoveredNum] = useState(1);
+  // ReturnTitle から前画面に戻る際に前画面が menuScreen か detailScreen かを保存する
   const [wasMenuScreen, setWasMenuScreen] = useState(false);
+  // ReturnTitle のラジオボタンの状態を保存する（デフォルトは yes にチェック）
   const [selectedOption, setSelectedOption] = useState("yes");
 
-  // useRef を使ってオーディオインスタンスを管理することでBGMの再生・停止を同一インスタンスで行う
-  const backgroundRef = useRef(new Audio(backgroundAudio));
-  backgroundRef.current.loop = true;
-
-  const titleScreenButton = new Audio(titleScreenButtonAudio);
-  const menuScreenButton = new Audio(menuScreenButtonAudio);
-  const detailScreenBButton = new Audio(detailScreenBButtonAudio);
-  const powerOn = new Audio(powerOnAudio);
-  const powerOff = new Audio(powerOffAudio);
-  const cursor = new Audio(cursorAudio);
-
-  // メニュー画面の項目を定義し、各コンポーネントとマッピング
+  // メニュー画面の項目
   const columns = ['PROFILE', 'CAREER', 'SKILL', 'DELIVERABLE'];
+  // 定義したメニュー画面の項目と各コンポーネントをマッピングさせる
   const columnsMap = {
     PROFILE: Profile,
     CAREER: Career,
@@ -53,49 +52,87 @@ const GameBoy = () => {
     DELIVERABLE: Deliverable
   };
   const ColumnComponent = columnsMap[columns[hoveredNum - 1]];
-
-  // 各項目のコンポーネント内でボタンによるスクロールを実現するためのref
+  // 詳細画面で 十字キー によるスクロールを実現する
   const menuScreenRef = useRef(null);
 
+
+  // ===================== ボタンクリック音・BGM ===================== //
+
+  // タイトル画面での A・Bボタン クリック
+  const titleScreenButton = new Audio(titleScreenButtonAudio);
+  // メニュー画面での Aボタン クリック
+  const menuScreenButton = new Audio(menuScreenButtonAudio);
+  // 詳細画面での Bボタン クリック
+  const detailScreenBButton = new Audio(detailScreenBButtonAudio);
+  // 電源ボタン クリック（オン）
+  const powerOn = new Audio(powerOnAudio);
+  // 電源ボタン クリック（オフ）
+  const powerOff = new Audio(powerOffAudio);
+  // 十字キー クリック
+  const cursor = new Audio(cursorAudio);
+  // 基本BGM
+  const backgroundRef = useRef(new Audio(backgroundAudio));
+  backgroundRef.current.loop = true;
+
+
+  // ================== ボタンクリック時の挙動 ================== //
+
+  // 電源ボタン
   const powerButtonClick = () => {
-    if (screenPower) {
-      // 電源オフ
+    if (screenPower) { // 電源オンの場合
       backgroundRef.current.pause();
       backgroundRef.current.currentTime = 0;
-
       powerOff.play();
-
-      // 画面判定のStateをリセット
-      setTitleScreen(true);
+      setTitleScreen(true); // 各Stateをリセット
       setMenuScreen(false);
       setDetailScreen(false);
-      setShowReturnTitle(false);
       setHoveredNum(1);
-    } else {
-      // 電源オン
+      setShowReturnTitle(false);
+      setSelectedOption("yes")
+    } else { // 電源オフの場合
       powerOn.play();
       backgroundRef.current.play();
     }
     setScreenPower(prevScreenPower => !prevScreenPower);
   };
-
+  // 電源ボタン クリック時にタイトル画面をフェードイン
+  useEffect(() => {
+    if (screenPower) {
+      setScreenStyle({
+        // 2秒かけてTitleコンポーネントのopacityを1に
+        transition: 'opacity 2s ease'
+      });
+      requestAnimationFrame(() => {
+        setScreenStyle(prevStyle => ({
+          ...prevStyle,
+          opacity: 1,
+        }));
+      });
+    } else {
+      setScreenStyle({ // 電源オフ時にopacityを再度0に設定
+        opacity: 0,
+      });
+    }
+  }, [screenPower]);
+  // Aボタン
   const buttonAClick = () => {
-    if (!screenPower) return;
+    if (!screenPower) return; // 電源オフの場合
 
-    if (titleScreen) {
+    if (titleScreen) { // タイトル画面の場合
         titleScreenButton.play();
         setMenuScreen(true);
         setTitleScreen(false);
-    } else if (menuScreen) {
+    } else if (menuScreen) { // メニュー画面の場合
         menuScreenButton.play();
         setDetailScreen(true);
         setMenuScreen(false);
         setShowReturnTitle(false);
-    } else if (showReturnTitle) {
+    } else if (showReturnTitle) { // リターン画面の場合
       if (selectedOption === "yes") {
         menuScreenButton.play();
         setTitleScreen(true);
         setShowReturnTitle(false);
+        setHoveredNum(1);
       } else if (selectedOption === "no") {
         menuScreenButton.play();
         setShowReturnTitle(false)
@@ -108,15 +145,15 @@ const GameBoy = () => {
       }
     }
   };
-
+  // Bボタン
   const buttonBClick = () => {
-    if (!screenPower) return;
+    if (!screenPower) return; // 電源オフの場合
 
-    if (titleScreen) {
+    if (titleScreen) { // タイトル画面の場合
         titleScreenButton.play();
         setMenuScreen(true);
         setTitleScreen(false);
-    } else if (showReturnTitle) {
+    } else if (showReturnTitle) { // リターン画面の場合
       detailScreenBButton.play();
       setShowReturnTitle(false);
       setSelectedOption("yes");
@@ -126,17 +163,18 @@ const GameBoy = () => {
         setDetailScreen(true)
       }
       setWasMenuScreen(false);
-    } else if (!menuScreen) {
+    } else if (detailScreen) { // 詳細画面の場合
       detailScreenBButton.play();
       setDetailScreen(false);
       setMenuScreen(true);
       setShowReturnTitle(false);
     }
   };
-
+  // セレクトボタン
   const selectButtonClick = () => {
-    if (!screenPower) return;
+    if (!screenPower) return; // 電源オフの場合
 
+    // タイトル画面 以外の場合は リターン画面 を表示させる
     if (!titleScreen) {
       setShowReturnTitle(true);
       if (menuScreen) {
@@ -148,84 +186,70 @@ const GameBoy = () => {
       }
     }
   };
-
   // 十字キーを押している間だけ border の色を変化させる
   const handleMouseDown = (event) => {
     event.target.style.borderColor = 'black';
   };
-
   // 十字キーを離すと border の色を元に戻す
   const handleMouseUp = (event) => {
     event.target.style.borderColor = '#555555';
   };
-
+  // 詳細画面で 十字キー 連打時にクリック音を連続して出せるようにする
+  const playCursorSound = () => {
+    cursor.pause();
+    cursor.currentTime = 0;
+    cursor.play();
+  };
+  // 十字キー（上）
   const topBottomClick = () => {
-    if (screenPower && menuScreen) {
+    if (!screenPower) return; // 電源オフの場合
+
+    if (menuScreen) { // メニュー画面の場合
       setHoveredNum(prevNum => (prevNum > 1 ? prevNum - 1 : columns.length));
       cursor.play();
-    }
-
-    // ボタンによるスクロール処理
-    if (menuScreenRef.current) {
-      menuScreenRef.current.scrollBy({ top: -80, behavior: 'smooth' });
+    } else if (detailScreen && ColumnComponent !== Profile) { // プロフィール以外の詳細画面の場合
+      playCursorSound();
+      if (menuScreenRef.current) { // 十字キー（上）で画面をスクロール
+          menuScreenRef.current.scrollBy({ top: -80, behavior: 'smooth' });
+        }
     }
   };
-
-  const leftBottomClick = () => {
-    if (showReturnTitle) {
-      cursor.play();
-      if (selectedOption === "yes") {
-        setSelectedOption("no");
-      } else if (selectedOption === "no") {
-        setSelectedOption("yes");
-      }
-    }
-  }
-
-  const rightBottomClick = () => {
-    if (showReturnTitle) {
-      cursor.play();
-      if (selectedOption === "yes") {
-        setSelectedOption("no");
-      } else if (selectedOption === "no") {
-        setSelectedOption("yes");
-      }
-    }
-  }
-
+  // 十字キー（下）
   const bottomBottomClick = () => {
-    if (screenPower && menuScreen) {
+    if (!screenPower) return; // 電源オフの場合
+
+    if (menuScreen) { // メニュー画面の場合
       setHoveredNum(prevNum => (prevNum < columns.length ? prevNum + 1 : 1));
       cursor.play();
-    }
-
-    // ボタンによるスクロール処理
-    if (menuScreenRef.current) {
-      menuScreenRef.current.scrollBy({ top: 80, behavior: 'smooth' });
+    } else if (detailScreen && ColumnComponent !== Profile) { // プロフィール以外の詳細画面の場合
+      playCursorSound();
+      if (menuScreenRef.current) { // 十字キー（下）で画面をスクロール
+        menuScreenRef.current.scrollBy({ top: 80, behavior: 'smooth' });
+      }
     }
   };
-
-  // タイトル画面のフェードイン処理
-  useEffect(() => {
-    if (screenPower) {
-      setScreenStyle({
-        // 2秒かけてTitleコンポーネントのopacityを1に変化させる
-        transition: 'opacity 2s ease'
-      });
-
-      requestAnimationFrame(() => {
-        setScreenStyle(prevStyle => ({
-          ...prevStyle,
-          opacity: 1,
-        }));
-      });
-    } else {
-      // 電源オフ時にopacityを再度0に設定
-      setScreenStyle({
-        opacity: 0,
-      });
+  // 十字キー（左）
+  const leftBottomClick = () => {
+    if (showReturnTitle) { // リターン画面の場合
+      cursor.play();
+      if (selectedOption === "yes") {
+        setSelectedOption("no");
+      } else if (selectedOption === "no") {
+        setSelectedOption("yes");
+      }
     }
-  }, [screenPower]);
+  }
+  // 十字キー（右）
+  const rightBottomClick = () => {
+    if (showReturnTitle) { // リターン画面の場合
+      cursor.play();
+      if (selectedOption === "yes") {
+        setSelectedOption("no");
+      } else if (selectedOption === "no") {
+        setSelectedOption("yes");
+      }
+    }
+  }
 
   return (
     <div className="gba">
@@ -244,9 +268,11 @@ const GameBoy = () => {
                 selectedOption={selectedOption} // 状態を渡す
                 onSelectOption={setSelectedOption} // 状態を変更する関数を渡す
               />
-            ) : (
+            ) : detailScreen ? (
               // メニュー画面にある各項目のコンポーネントを呼び出す
               <ColumnComponent />
+            ) : (
+              <></>
             )}
           </div>
         ) : (
