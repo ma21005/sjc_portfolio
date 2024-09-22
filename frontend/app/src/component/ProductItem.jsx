9984a1543b17a60ca04b51bbf070dcfa89796409
ma21005 <hockey0513hockey@gmail.com>
2024-09-22 18:46:14 +0900

    Product ページ更新

--- frontend/app/src/component/ProductItem.jsx
+++ frontend/app/src/component/ProductItem.jsx
@@ -8,22 +8,28 @@ const ProductItem = (props) => {
       <div className={`product nes-container is-rounded is-dark ${isHovered
           ? 'product-hovered'
           : ''}`}>
-        {isHovered && (
-          <div className='up-arrow-container'>
-            {!isFirst && (
-              <p className="up-arrow">▲</p>
-            )}
+        <div className='up-arrow-container'>
+          {isHovered && !isFirst && (
+            <p className="up-arrow">▲</p>
+          )}
+        </div>
+        <div className='product-name-container'>
+          {product.name}
+        </div>
+        <div className='product-description-container'>
+          {product.description}
+        </div>
+        <div className='product-url-and-down-arrow-container'>
+          <div className='product-url-container'>
+            <i className="nes-icon google"></i>
+            <button type="button" className="url-button nes-btn is-primary">Press A Button</button>
           </div>
-        )}
-        {product.name}
-        {product.description}
-        {isHovered && (
           <div className='down-arrow-container'>
-            {!isLast && (
-              <p className="down-arrow">▲</p>
-            )}
+          {isHovered && !isLast && (
+            <p className="down-arrow">▲</p>
+          )}
           </div>
-        )}
+        </div>
       </div>
     </div>
   );
